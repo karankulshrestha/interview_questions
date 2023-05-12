f2e9372011bb2caf725f7241d6de430df637389b
Karan Kulshestha <karan2111001@akgec.ac.in>
2023-05-12 21:07:57 +0530

    Solving Questions With Brainpower

--- dynamic_programming/Solving Questions With Brainpower.cpp
+++ dynamic_programming/Solving Questions With Brainpower.cpp
@@ -3,7 +3,10 @@
     companies: Google, Oracle, Amazon
     qlink: https://leetcode.com/problems/solving-questions-with-brainpower/description/
     rating: easy-medium
+    tag: dp, arrays
 */
+
+// Top Down Approach
 
 class Solution {
 public:
@@ -23,5 +26,22 @@ public:
         n = q.size();
         vector<long long> t(n, -1);
         return solve(0, q, t);
+    }
+};
+
+// Bottom Up Approach
+
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& q) {
+        int n = q.size();
+        if(n == 1) {
+            return q[0][0];
+        };
+        vector<long long> t(200001, 0);
+        for(int i = n-1; i>=0; i--) {
+            t[i] = max(q[i][0] + t[i + q[i][1] + 1], t[i + 1]);
+        };
+        return t[0];
     }
 };
