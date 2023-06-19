dbf677b89a578616322df171058d2f6af6dd9082
Karan Kulshestha <karan2111001@akgec.ac.in>
2023-06-19 16:09:00 +0530

    Longest Common Sub Sequence

--- dynamic_programming/longest common subsequence.cpp
+++ dynamic_programming/longest common subsequence.cpp
@@ -28,3 +28,38 @@ public:
         return solve(s1, s2, 0, 0);
     }
 };
+
+// Bottom up approach
+
+class Solution {
+public:
+    int n, m;
+    int solve(string& s1, string& s2, int i , int j) {
+        vector<vector<int>> t(n + 1, vector<int>(m + 1));
+
+        for(int i = 0; i<m; i++) {
+            t[0][i] = 0;
+        };
+
+        for(int j = 0; j<n; j++) {
+            t[j][0] = 0;
+        };
+
+        for(int i = 1; i<n + 1; i++) {
+            for(int j = 1; j<m + 1; j++) {
+                if(s1[i - 1] == s2[j - 1]) {
+                    t[i][j] = 1 + t[i-1][j-1];
+                } else {
+                    t[i][j] = max(t[i][j-1], t[i-1][j]);
+                }
+            }
+        }
+        return t[n][m];
+    };
+
+    int longestCommonSubsequence(string s1, string s2) {
+        n = s1.length();
+        m = s2.length();
+        return solve(s1, s2, 0, 0);
+    }
+};
